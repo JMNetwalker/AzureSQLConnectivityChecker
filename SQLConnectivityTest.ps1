be8e2b4f4d5ab8c73db857d911e9bd12d805f648
Sérgio Fonseca <sergio@fonsecanet.com>
2019-02-20 10:08:25 +0000

    Update SQLConnectivityTest.ps1
    Variable for log output

--- SQLConnectivityTest.ps1
+++ SQLConnectivityTest.ps1
@@ -5,6 +5,7 @@ $Password = "xxxxxx"
 $Pooling = $true
 $NumberExecutions =10
 $File = "E:\TSQL.SQL"
+$OutputLog = "C:\TEMP\LOG.txt"
 
 cls
 
@@ -51,28 +52,28 @@ cls
        $connection.Close()
        $end = get-date
        $data = $connection.RetrieveStatistics()
-       write-Output "-------------------------" | Out-File e:\my.log -Append -width 300 
-       write-Output ("Query                 :  " + $query[$iQuery]) | Out-File e:\my.log -Append -width 300
-       write-Output ("Iteration             :  " +$i) | Out-File e:\my.log -Append  -Width 300
-       write-Output ("Time required (ms)    :  " +(New-TimeSpan -Start $start -End $end).TotalMilliseconds) | Out-File e:\my.log -Append  -Width 300
-       write-Output ("NetworkServerTime (ms):  " +$data.NetworkServerTime) | Out-File e:\my.log -Append  -Width 300
-       write-Output ("Execution Time (ms)   :  " +$data.ExecutionTime) | Out-File e:\my.log -Append  -Width 300
-       write-Output ("Connection Time       :  " +$data.ConnectionTime) | Out-File e:\my.log -Append -Width 300
-       write-Output ("ServerRoundTrips      :  " +$data.ServerRoundtrips) | Out-File e:\my.log -Append -Width 300
-       write-Output ("BuffersReceived       :  " +$data.BuffersReceived) | Out-File e:\my.log -Append -Width 300
-       write-Output ("SelectRows            :  " +$data.SelectRows) | Out-File e:\my.log -Append -Width 300
-       write-Output ("SelectCount           :  " +$data.SelectCount) | Out-File e:\my.log -Append -Width 300
-       write-Output ("BytesSent             :  " +$data.BytesSent) | Out-File e:\my.log -Append -Width 300
-       write-Output ("BytesReceived         :  " +$data.BytesReceived) | Out-File e:\my.log -Append -Width 300
-       write-Output "-------------------------" | Out-File e:\my.log -Append  -Width 300
+       write-Output "-------------------------" | Out-File $OutputLog -Append -width 300 
+       write-Output ("Query                 :  " + $query[$iQuery]) | Out-File $OutputLog -Append -width 300
+       write-Output ("Iteration             :  " +$i) | Out-File $OutputLog -Append  -Width 300
+       write-Output ("Time required (ms)    :  " +(New-TimeSpan -Start $start -End $end).TotalMilliseconds) | Out-File $OutputLog -Append  -Width 300
+       write-Output ("NetworkServerTime (ms):  " +$data.NetworkServerTime) | Out-File $OutputLog -Append  -Width 300
+       write-Output ("Execution Time (ms)   :  " +$data.ExecutionTime) | Out-File $OutputLog -Append  -Width 300
+       write-Output ("Connection Time       :  " +$data.ConnectionTime) | Out-File $OutputLog -Append -Width 300
+       write-Output ("ServerRoundTrips      :  " +$data.ServerRoundtrips) | Out-File $OutputLog -Append -Width 300
+       write-Output ("BuffersReceived       :  " +$data.BuffersReceived) | Out-File $OutputLog -Append -Width 300
+       write-Output ("SelectRows            :  " +$data.SelectRows) | Out-File $OutputLog -Append -Width 300
+       write-Output ("SelectCount           :  " +$data.SelectCount) | Out-File $OutputLog -Append -Width 300
+       write-Output ("BytesSent             :  " +$data.BytesSent) | Out-File $OutputLog -Append -Width 300
+       write-Output ("BytesReceived         :  " +$data.BytesReceived) | Out-File $OutputLog -Append -Width 300
+       write-Output "-------------------------" | Out-File $OutputLog -Append  -Width 300
       }
      }
     catch
    {
-    Write-Output -ForegroundColor DarkYellow "You're WRONG" | Out-File e:\my.log -Append -Width 300
-    Write-Output -ForegroundColor Magenta $Error[0].Exception | Out-File e:\my.log -Append -Width 300
+    Write-Output -ForegroundColor DarkYellow "You're WRONG" | Out-File $OutputLog -Append -Width 300
+    Write-Output -ForegroundColor Magenta $Error[0].Exception | Out-File $OutputLog -Append -Width 300
    }
    
 }
-write-Output ("Time spent (ms) Procces :  " +$sw.elapsed) | Out-File e:\my.log -Append -Width 300
-write-Output ("Review: https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql/provider-statistics-for-sql-server") | Out-File e:\my.log -Append -Width 300
+write-Output ("Time spent (ms) Procces :  " +$sw.elapsed) | Out-File $OutputLog -Append -Width 300
+write-Output ("Review: https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql/provider-statistics-for-sql-server") | Out-File $OutputLog -Append -Width 300
